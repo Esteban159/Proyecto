12a0e42bfe70798c1dff7597c3cb94964633b942
Esteban159 <esteban.adrian.159@gmail.com>
2015-05-23 09:32:53 -0500

    Plus_Market

---
+++ Project_PlusMarket/vistas/js/project.js
@@ -0,0 +1,29 @@
+function changeIframe1() {
+  	var ventana = document.getElementById('myIframe');
+    ventana.src = "almacen.html";
+}
+
+function changeIframe2() {
+  	var ventana = document.getElementById('myIframe');
+    ventana.src = "stock.html";
+}
+
+function changeIframe3() {
+  	var ventana = document.getElementById('myIframe');
+	ventana.src = "historial.html";
+}
+
+function changeIframe4() {
+  	var ventana = document.getElementById('myIframe');
+    ventana.src = "contabilidad.html";
+}
+
+function changeIframe5() {
+  	var ventana = document.getElementById('myIframe');
+	ventana.src = "reg_productos.html";
+}
+
+function changeIframe6() {
+    var ventana = document.getElementById('myIframe');
+  ventana.src = "atencion.html";
+}
